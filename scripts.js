17a9a23f95c71e831d78ce17907c1a79d8f1bd97
jendos-kgd <edanileyko@gmail.com>
2020-09-14 13:07:09 +0200

    made script for change header background

--- scripts.js
+++ scripts.js
@@ -3,13 +3,13 @@
 $(document).ready(function () {
   dropdownMenu();
   closeDropdown();
+  changeSLides();
+  console.log($('.main_game_item').css('width'));
 })
-
 
 function closeDropdown() {
   $(document).click(function (e){ // событие клика по странице
     if ($('#games').css('display') == 'block' || $('#consoles').css('display') == 'block' || $('#my_xbox').css('display') == 'block') { // если открыт какой-то dropdown
-
       if (!$('.dropdown_menu').is(e.target) && // если клик сделан не по элементу
           $('.dropdown_menu').has(e.target).length === 0 && // если клик сделан не по вложенным элементам
           !$('.dropdown_title').is(e.target)) {  //если клик сделан не по кнопке открытия dropdown
@@ -43,4 +43,36 @@ function dropdownMenu() {
       $('#my_xbox').css('display', 'none');
     };
   });
+}
+function changeSLides() {
+  // Определяем переменные:
+  let slideRight = $('.main_game_item').css('width');
+  let slideOverRight = parseInt(slideRight) * 2 + 'px' ;
+  let slideLeft = '-' + slideRight;
+  let slideOverLeft = +parseInt(slideLeft) * 2 + 'px';
+  let show = '0px';
+
+  $('#next_slide').click(function () {
+    if ($('#first_slide').css('left') == show) {
+      $('#first_slide').css('left', slideLeft);
+      $('#second_slide').css('left', show);
+      $('#third_slide').css('left', slideRight);
+    } else if ($('#second_slide').css('left') == show) {
+      $('#first_slide').css('left', slideOverLeft);
+      $('#second_slide').css('left', slideLeft);
+      $('#third_slide').css('left', show);
+    }
+  })
+
+  $('#prev_slide').click(function () {
+    if ($('#second_slide').css('left') == show) {
+      $('#first_slide').css('left', show);
+      $('#second_slide').css('left', slideRight);
+      $('#third_slide').css('left', slideOverRight);
+    } else if ($('#third_slide').css('left') == show) {
+      $('#first_slide').css('left', slideLeft);
+      $('#second_slide').css('left', show);
+      $('#third_slide').css('left', slideRight);
+    }
+  })
 }
